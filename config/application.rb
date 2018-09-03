8c4819c0280711fc8933ce1fa9e055eeb86637f9
calebkeene <caleb@hashbang.co.nz>
2018-09-03 17:45:08 +1200

    remove cruft from app conf

--- config/application.rb
+++ config/application.rb
@@ -10,25 +10,14 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module FlyBuysGamblerRails
   class Application < Rails::Application
     config.load_defaults 5.2
     config.api_only = false
-
-    #config.session_store :cookie_store, key: "_fly_buys_gambler_session_#{Rails.env}"
     config.middleware.use ActionDispatch::Cookies # Required for all session management
-    #config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
-
-    #config.middleware.use ActionDispatch::Cookies # will need this for 'logging in' users
-    #config.middleware.use ActionDispatch::Session::CookieStore, key: '_member_session', expire_after: 20.minutes
-    #config.middleware.insert_after ActionDispatch::Cookies, ActionDispatch::Session::CookieStore
-    #config.middleware.insert_after ActiveRecord::QueryCache, ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
   end
 end
